01b83c4671a6150c72b12e72de55ad9e6f7348a8
George Sivtsev <gumben7@gmail.com>
2024-09-18 10:33:51 +0700

    Teachers and Students added to Subject

--- Models/Subject.cs
+++ Models/Subject.cs
@@ -2,5 +2,7 @@
     public class Subject {
         public int Id { get; set; }
         public required string Name { get; set; }
+        public ICollection<Teacher> Teachers { get; set; } = [];
+        public ICollection<Student> Students { get; set; } = [];
     }
 }
